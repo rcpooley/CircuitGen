b41e77925192bcf9b91e90f127fd0564f9d3dba1
Robert Pooley <coolosity1998@gmail.com>
2017-01-21 21:00:10 -0500

    Added flask server

---
+++ FlaskServer/server.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+import socket
+app = Flask(__name__)
+
+xorCircuitJson = '{"components":[3,3,0,0,2,2,1],"connections":[{"toid":0,"fromid":-1,"tonode":0},{"toid":1,"fromid":-1,"tonode":0},{"toid":2,"fromid":5,"tonode":0},{"toid":2,"fromid":1,"tonode":1},{"toid":3,"fromid":0,"tonode":0},{"toid":3,"fromid":4,"tonode":1},{"toid":4,"fromid":1,"tonode":0},{"toid":5,"fromid":0,"tonode":0},{"toid":6,"fromid":2,"tonode":0},{"toid":6,"fromid":3,"tonode":1},{"toid":-1,"fromid":6,"tonode":0}]}';
+
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return sendstring(xorCircuitJson)
+
+
+@app.route("/")
+def hello():
+    return sendstring(xorCircuitJson)
+
+
+def sendstring(str):
+    sock = socket.socket()
+    sock.connect(("localhost", 7276))
+    sock.send(str.encode(encoding='UTF-8'))
+    sock.settimeout(5)
+
+    try:
+        ret = sock.recv(1024)
+    except socket.timeout:
+        ret = "[timed out]"
+
+    sock.close()
+    return ret
+
+if __name__ == "__main__":
+    app.run(debug=True)
